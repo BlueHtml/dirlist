386e6355dd08ffaa44f4ab4682f2acfe0371ec62
fly <luxiaofeng006@qq.com>
2025-12-18 17:19:55 +0800

    使用 CSS 实现粘性页脚，删除 fix_footer() 函数。

--- _dir/page/footer.php
+++ _dir/page/footer.php
@@ -5,17 +5,6 @@ if(!defined('DIR_INIT'))exit();
 <footer class="footer card-footer mt-3" id="footer">
 <span> Copyright &copy;<?php echo date('Y')?> <?php echo $conf['title']?>  <?php echo $conf['footer']?></span>
 </footer>
-<script>
-function fix_footer(){
-    var body_height = document.getElementById("navbar").offsetHeight + document.getElementById("main").offsetHeight;
-    var foot_height = document.getElementById("footer").offsetHeight;
-    var win_height = window.innerHeight;
-    if(body_height + foot_height > win_height){
-        document.getElementById("footer").className += ' position-relative';
-    }
-}
-fix_footer()
-</script>
 <?php }?>
 <script src="<?php echo $cdnpublic?>jquery/3.6.4/jquery.min.js"></script>
 <script src="<?php echo $cdnpublic?>popper.js/1.16.1/umd/popper.min.js"></script>
